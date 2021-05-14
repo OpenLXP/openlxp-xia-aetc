04e6176e4e5fa2a0946b1857def3dfd6611e4e1b
KarenAJ <kjijo@deloitte.com>
2021-05-14 11:42:30 -0400

    metadata preprocessing reading metadata from s3 bucket added

--- app/preprocess/management/commands/metadata_preprocessing.py
+++ app/preprocess/management/commands/metadata_preprocessing.py
@@ -1,4 +1,7 @@
 import logging
+import boto3
+import pandas as pd
+import io
 
 from django.core.management.base import BaseCommand
 
@@ -10,5 +13,17 @@ class Command(BaseCommand):
     extraction in EVTVL process"""
 
     def handle(self, *args, **options):
-        """ Extracting raw metadata and process to further
-        extraction in EVTVL process"""
+        """ Extracting raw metadata from tables for further process"""
+
+        # get object and file (key) from bucket
+        bucket = 'xsraetc'
+        file_name = 'AFCS_ETCA_Data.xlsx'
+        client = boto3.client('s3')
+        csv_obj = client.get_object(Bucket=bucket, Key=file_name)
+        table_data = csv_obj['Body'].read()
+        # Ingesting as dataframe values
+        ETCA_BCI_df = pd.read_excel(io.BytesIO(table_data), sheet_name='ETCA_BCI', engine='openpyxl')
+        ETCA_BCI_AETC_df = pd.read_excel(io.BytesIO(table_data), sheet_name='ETCA_BCI_AETC', engine='openpyxl')
+        # ETCA_BCI_AETC_df.to_csv('file_name.csv')
+
+
