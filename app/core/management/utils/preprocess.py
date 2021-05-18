1f03555088ebae725ac490acd08caf89a809604c
KarenAJ <kjijo@deloitte.com>
2021-05-18 18:21:09 -0400

    Preprocessing for AETC etca_bci and etca_bci_aetc

--- app/core/management/utils/preprocess.py
+++ app/core/management/utils/preprocess.py
@@ -2,7 +2,7 @@ import logging
 import boto3
 import pandas as pd
 import io
-
+import numpy as np
 from django.core.management.base import BaseCommand
 
 logger = logging.getLogger('dict_config_logger')
@@ -19,6 +19,21 @@ def get_aws_details():
     return table_data
 
 
+def get_list_table_names():
+    """Function to return table names"""
+    table_list = ['ETCA_Course_Paragraph', 'ETCA_Course_Paragraph_711HPW',
+                  'ETCA_Course_Paragraph_ACC', 'ETCA_Course_Paragraph_AETC',
+                  'ETCA_Course_Paragraph_AETC2', 'ETCA_Course_Paragraph_AETC3',
+                  'ETCA_Course_Paragraph_AETC4', 'ETCA_Course_Paragraph_AFIT',
+                  'ETCA_Course_Paragraph_AFSOC', 'ETCA_Course_Paragraph_ANG',
+                  'ETCA_Course_Paragraph_AU', 'ETCA_Course_Paragraph_FT',
+                  'ETCA_Course_Paragraph_LT1K', 'ETCA_Course_Paragraph_Org_1K',
+                  'ETCA_Course_Paragraph_SAFIG',
+                  'ETCA_Course_Paragraph_USAFEC']
+
+    return table_list
+
+
 def get_etca_bci():
     """ Extracting raw metadata from tables for further process"""
     table_data = get_aws_details()
@@ -26,41 +41,41 @@ def get_etca_bci():
     ETCA_BCI_df = pd.read_excel(io.BytesIO(table_data), sheet_name='ETCA_BCI',
                                 engine='openpyxl')
 
-    # Getting rows only when organization is AETC
-    ETCA_BCI_df = ETCA_BCI_df[ETCA_BCI_df['Organization'] == 'AETC']
-
-    # Getting null and not null dataframes
-    ETCA_BCI_df_is_null = ETCA_BCI_df[ETCA_BCI_df["BCI_ID"].isnull()]
-    ETCA_BCI_df_not_null = ETCA_BCI_df[ETCA_BCI_df["BCI_ID"].notnull()]
-
-    # Setting up index to 'BCI_ID'
-    ETCA_BCI_df_not_null = ETCA_BCI_df_not_null.set_index('BCI_ID')
-
-    # Converting both dataframe into json
-    ETCA_BCI_df_is_null_dict = ETCA_BCI_df_is_null.to_dict(orient='index')
-    ETCA_BCI_df_not_null_dict = ETCA_BCI_df_not_null.to_dict(orient='index')
-
-    # print(ETCA_BCI_df_not_null_dict)
-
-    return ETCA_BCI_df_not_null_dict
+    ETCA_BCI_df = get_paragraph_data(ETCA_BCI_df)
+    ETCA_BCI_df['Resp_Org_ID'] = 'ETCA_BCI'
+    return ETCA_BCI_df
+    # df.to_csv('etca_bci.csv')
 
 
-def get_paragraph_data():
+def get_etca_bci_aetc():
+    """ Extracting raw metadata from tables for further process"""
     table_data = get_aws_details()
-    ETCA_BCI_df_not_null_dict = get_etca_bci()
-    ETCA_Course_Paragraph_df = pd.read_excel(io.BytesIO(table_data),
-                                             sheet_name='ETCA_Course_Paragraph',
-                                             engine='openpyxl',
-                                             usecols=['BCI_ID',
-                                                      'Paragraph_Heading',
-                                                      'Paragraph_Text'])
-    dict_ETCA_BCI = ETCA_Course_Paragraph_df.to_dict(orient='index')
-    logger.info(ETCA_Course_Paragraph_df)
-    #
-    # for i in ETCA_BCI_df_not_null_dict:
-    #     print(ETCA_BCI_df_not_null_dict(14223.0))
-    for index, row in ETCA_Course_Paragraph_df.iterrows():
-        key =float(row['BCI_ID'])
-        #print(type(key))
-        print((ETCA_BCI_df_not_null_dict[key]))
-        temp_dict = {row['Paragraph_Heading']: row['Paragraph_Text']}
+    # Ingesting as dataframe values
+    ETCA_BCI_AETC_df = pd.read_excel(io.BytesIO(table_data),
+                                     sheet_name='ETCA_BCI_AETC',
+                                     engine='openpyxl')
+    ETCA_BCI_AETC_df = get_paragraph_data(ETCA_BCI_AETC_df)
+    ETCA_BCI_AETC_df['Resp_Org_ID'] = 'ETCA_BCI_AETC'
+    return ETCA_BCI_AETC_df
+    # df.to_csv('etca_bci_aetc.csv')
+
+
+def get_paragraph_data(core_table_df):
+    table_data = get_aws_details()
+    # Making a list of paragraph tables sheet names
+    table_list = get_list_table_names()
+    for table_name in table_list:
+        ETCA_Course_Paragraph_df = pd.read_excel(io.BytesIO(table_data),
+                                                 sheet_name=table_name,
+                                                 engine='openpyxl',
+                                                 usecols=['BCI_ID',
+                                                          'Paragraph_Heading',
+                                                          'Paragraph_Text'])
+        ETCA_Course_Paragraph_df = ETCA_Course_Paragraph_df[
+            ETCA_Course_Paragraph_df["BCI_ID"].notnull()]
+        for index, row in ETCA_Course_Paragraph_df.iterrows():
+            val = core_table_df.index[core_table_df['BCI_ID'] == row['BCI_ID']]
+            core_table_df.loc[core_table_df.index[val],
+                              row['Paragraph_Heading']] = row['Paragraph_Text']
+    return core_table_df
+    # core_table_df.to_csv('filename.csv')
